bd19e23edbf8135658cf12b1e30158be474607e2
thibault.lazert <thibault.lazert@gmail.com>
2012-03-27 08:18:25 +0000

    pour bool

--- projet/src/Case.h
+++ projet/src/Case.h
@@ -4,10 +4,10 @@
 typedef struct
 {
 	/* il y a un pion ici */
-	bool libre;
+	int libre;
 	/* définit si une case est accessible ou non
 	(par exemple pour faire des trous sur le plateau) */
-	bool accessible;
+	int accessible;
 	/* numéro du joueur qui possède le pion */
 	int joueur;
 	/* coordonnées du pion sur le support */
