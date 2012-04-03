dcbda1611890329559a3ef2a75c530e4263719ae
thibault.lazert <thibault.lazert@gmail.com>
2012-04-03 06:19:57 +0000

    Redimensionnement des images et modification de parametres.h

--- projet/src/parametres.h
+++ projet/src/parametres.h
@@ -5,7 +5,9 @@
 	Paramètres pour les images à charger.
 */
 
-	#define PION_JOUEUR_1 "data/Texture1/Rubis.png"
-	#define PION_JOUEUR_2 "date/Texture1/Perle.png"
+	#define PION_JOUEUR_1 "../data/Texture1/Rubis.png"
+	#define PION_JOUEUR_2 "../date/Texture1/Perle.png"
+	#define CASE_VIDE "../data/Texture1/Case.png"
+	#define CASE_JOUEABLE "../data/Texture1/Case2.png"
 
 #endif
