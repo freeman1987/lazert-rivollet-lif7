01c9eeebb6234691ebe8c00c25432dc02d77e2ba
GodifreyI@gmail.com <GodifreyI@gmail.com@c1644284-d403-b3e4-6d7a-00eeac78b690>
2012-04-03 08:16:37 +0000

    plateau

--- projet/src/Plateau.c
+++ projet/src/Plateau.c
@@ -7,7 +7,7 @@
 void plateauInit(Plateau* p, int capa)
 {
     p->capacite = capa;
-    p->places_libre = 0;
+    p->places_libres = 0;
 
     p->support = (Case **) malloc(sizeof(Case)*capa);
     if(p->support==0)
@@ -21,4 +21,18 @@ void plateauInit(Plateau* p, int capa)
 int getPlacesLibres(const Plateau* p)
 {
     return p->places_libres;
+}
+
+void affichePiece(Plateau* p)
+{
+    int i;
+    Case* ca;
+    for(i=0;i<p->capacite;i++)
+    {
+        ca = p->support[i];
+                if(==1)
+        dessinepion1(ca[i].X,ca[i].Y)
+        else
+        dessinepion2(ca[i].X,ca[i].Y)
+    }
 }
