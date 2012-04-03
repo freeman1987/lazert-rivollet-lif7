6468d45f4f207e11d352b765ea1576319bd76d9e
thibault.lazert <thibault.lazert@gmail.com>
2012-04-03 06:00:12 +0000

    Paramètres

---
+++ projet/src/parametres.h
@@ -0,0 +1,11 @@
+#ifndef _PARAMETRES
+#define _PARAMETRES
+
+/**
+	Paramètres pour les images à charger.
+*/
+
+	#define PION_JOUEUR_1 "data/Texture1/Rubis.png"
+	#define PION_JOUEUR_2 "date/Texture1/Perle.png"
+
+#endif
