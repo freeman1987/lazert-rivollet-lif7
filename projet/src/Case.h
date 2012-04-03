e5ae1c1a1655a22275790acef4744273442ca31c
thibault.lazert <thibault.lazert@gmail.com>
2012-04-03 06:52:02 +0000

    Fonctions case

--- projet/src/Case.h
+++ projet/src/Case.h
@@ -17,4 +17,37 @@ typedef struct
 	int posY;
 }Case;
 
+
+/**
+    @brief Definit quel joueur a une piece sur la case
+
+    @param [IN,OUT] c : pointeur sur la case à modifier
+    @param [IN] joueur : numero 1 ou 2 du joueur. Mettre 0 rendre la case libre
+
+    @return void
+*/
+void changeJoueur(Case* c, int joueur);
+
+
+/**
+    @brief Donner une position x,y a une case
+
+    @param [IN,OUT] c : pointeur sur la case à modifier
+    @param [IN] x : position horizontale
+    @param [IN] y : position verticale
+
+    @return void
+*/
+void setPos(const Case* c, int x, int y);
+
+
+/**
+    @brief Savoir si une case est libre
+
+    @param [IN] case : pointeur sur case
+
+    @return 1 si la case et libre et 0 sinon
+*/
+int getLibre(const Case* c);
+
 #endif
