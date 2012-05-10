095975ca2198881291d41bdcc40fd939ff8bc8fe
GodifreyI@gmail.com <GodifreyI@gmail.com@c1644284-d403-b3e4-6d7a-00eeac78b690>
2012-05-10 12:46:04 +0000

    gros changement

---
+++ projet/src/jeu.h
@@ -0,0 +1,17 @@
+#ifndef _JEU
+#define _JEU
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#include "parametres.h"
+#include "Case.h"
+#include "Plateau.h"
+
+/** \brief Cette fonction lance le jeu.
+ *
+ *  \return void
+ *
+ */
+void Jouer();
