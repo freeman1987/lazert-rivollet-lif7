e473159e81b9fce9a1122a9a234fa3f86158ebc4
thibault.lazert@gmail.com <thibault.lazert@gmail.com@c1644284-d403-b3e4-6d7a-00eeac78b690>
2012-05-10 12:58:17 +0000

    Menu h

---
+++ projet/src/menuprincipal.h
@@ -0,0 +1,19 @@
+#ifndef _MENUPRINCIPAL
+#define _MENUPRINCIPAL
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "parametres.h"
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+/**
+    @brief Affiche le menu principal
+
+    @return int : 0 pour quitter et 1 pour lancer le jeu
+*/
+int menuPrincipal();
+
+#endif
