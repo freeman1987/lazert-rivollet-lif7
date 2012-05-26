5f642a6fb7e72a9398b9fe2223b77b812c5c1c63
thibault.lazert <thibault.lazert@gmail.com>
2012-05-26 13:23:22 +0000

    Revision du main pour le test de régression, amélio aff

--- projet/src/main.c
+++ projet/src/main.c
@@ -10,17 +10,37 @@
 
 
 /**
+    @brief Fonction d'appel des tests de regression
+
+    @param void
+
+    @return int 0
+*/
+int regression(void)
+{
+    printf("\n\n-----------------------\n| TESTS DE REGRESSION |\n-----------------------\n| \n");
+    caseTestRegression();
+
+    printf("| \n-----------------------\n\n");
+    return 0;
+}
+
+/**
     @brief Fonction du jeu
+
+    @param appel avec "r" pour les tests de regression
 
     @return int
 */
-int main0(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+    if(argc>1 && argv[1][0]=='r') /* appel du test de régression */
+    {
+        regression();
+        return 0;
+    }
 
-
-    int rejouer;
     int contreordinateur, niveauordinateur, plateau;
-    rejouer = 1;
 
     /* paramètres de jeu par défaut */
     contreordinateur = 1;
@@ -31,11 +51,5 @@ int main0(int argc, char *argv[])
     {
         Jouer( contreordinateur, niveauordinateur, plateau);
     }
-    return 0;
-}
-
-int main(void)
-{
-    caseTestRegression();
     return 0;
 }
