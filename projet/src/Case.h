6baa66eafe692bba321e1614a543dd90a5a24485
thibault.lazert@gmail.com <thibault.lazert@gmail.com@c1644284-d403-b3e4-6d7a-00eeac78b690>
2012-05-27 14:57:57 +0000

    Fin de la documentation pour Doxygen

--- projet/src/Case.h
+++ projet/src/Case.h
@@ -8,7 +8,7 @@
 
 /**
     @struct Case
-    @brief Structure pour les cases du plateau de jeu
+    @brief <b>Structure pour les cases du plateau de jeu</b>
 */
 typedef struct
 {
@@ -20,29 +20,30 @@ typedef struct
 }Case;
 
 /**
-    @brief alloue de la memoire dans le tas et retourne l'adresse
+    @brief <b>Initialiser une case.</b><br />
+    Alloue de la memoire dans le tas et retourne l'adresse
 
-    @param void
-
-    @return pointeur sur case
+    @return pointeur Case
 */
 Case* caseInit(void);
 
 
 /**
-    @brief Donner une position x,y a une case
+    @brief <b>Déterminer une position x,y a une case.</b><br />
+    Fixer les coordonnées d'une case.
 
     @param [IN,OUT] c : pointeur sur la case à modifier
     @param [IN] x : position horizontale
     @param [IN] y : position verticale
 
-    @return void
+    @return rien
 */
 void caseSetPos(Case* c, int x, int y);
 
 
 /**
-    @brief Savoir si une case est libre
+    @brief <b>Savoir si une case est libre.</b><br />
+    Savoir si une case est libre ou occupée par un joueur.
 
     @param [IN] case : pointeur sur case
 
@@ -52,18 +53,20 @@ int caseGetLibre(const Case* c);
 
 
 /**
-    @brief Rendre libre ou non une case
+    @brief <b>Rendre libre ou non une case</b><br />
+    Si val vaut 0, la case devient libre.<br />
+    Dans tous les cas, le pion du joueur est effacé.
 
     @param [IN,OUT] c : pointeur sur la case a modifier
     @param [IN] val : booléen : libre ou non
 
-    @return void
+    @return rien
 */
 void caseSetLibre(Case* c, int val);
 
 
 /**
-    @brief Retourne le joueur qui a un pion sur la case
+    @brief <b>Retourne le joueur qui a un pion sur la case.</b>
 
     @param [IN] c : pointeur sur case
 
@@ -73,7 +76,8 @@ int caseGetJoueur(const Case* c);
 
 
 /**
-    @brief Modifier le joueur qui a un pion sur la case
+    @brief <b>Modifier le joueur qui a un pion sur la case</b><br />
+    Changer la valeur du joueur qui est sur la case.
 
     @param [IN,OUT] c : pointeur sur case
     @param [IN] j : numéro du joueur
@@ -84,17 +88,19 @@ void caseSetJoueur(Case* c, int j);
 
 
 /**
-    @brief Retourne la coordonnee X
+    @brief <b>Retourne la coordonnée x.</b><br />
+    Abscisse la case sur la plateau.
 
     @param [IN] c : pointeur sur case
 
-    @return coordonnee entiere
+    @return coordonnée entiere
 */
 int caseGetX(const Case* c);
 
 
 /**
-    @brief Retourne la coordonnee Y
+    @brief <b>Retourne la coordonnée y.</b><br />
+    Ordonnée de la case.
 
     @param [IN] c : pointeur sur case
 
@@ -104,33 +110,34 @@ int caseGetY(const Case* c);
 
 
 /**
-    @brief Comparer les coordonnées en paramètres avec celles de la case
+    @brief <b>Tester les coordonnées</b><br />
+    Comparer les coordonnées en paramètres avec celles de la case.
 
     @param [IN] c : Pointeur sur Case
     @param [IN] sx : coordonnée horizontale
     @param [IN] sy : coordonnée verticale
 
-    @return int (booléen)
+    @return booléen (0 ou 1)
 */
 int caseTestCoordonnees(Case* c,int sx,int sy);
 
 
 /**
-    @brief Détruire la case pour libérer la mémoire.
+    @brief <b>Détruire la case</b><br />
+    Libérer la mémoire.
 
     @param [IN] c : Pointeur sur la case à détruire
 
-    @return void
+    @return rien
 */
 void caseTestament(Case* c);
 
 
 /**
-    @brief Test de régression pour vérifier le module Case
+    @brief <b>Test de non-régression de Case</b><br />
+    Vérifier le module Case en appelant les différentes fonctions.
 
-    @param void
-
-    @return void
+    @return rien
 */
 void caseTestRegression(void);
 
